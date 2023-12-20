c7604f8f40c93c076c7ab2532b0eeea0be859a7b
kazungudev <kazungudev@gmail.com>
2023-12-20 15:54:02 +0300

    Initial commit

---
+++ lib/features/detail/controller/overview.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_getx_the_moviedb/core/base/index.dart';
+import 'package:get/get.dart';
+
+class OverviewController extends BaseController<dynamic> {
+  OverviewController();
+
+  final RxBool flag = true.obs;
+  final RxString visibleText = ''.obs;
+  final RxString invisibleText = ''.obs;
+
+  @override
+  void onHidden() {
+    // TODO: implement onHidden
+  }
+}
